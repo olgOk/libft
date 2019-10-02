277a9e8a480c88622e4986714606eec38afb5e5a
Olga Okrut <46659064+olgOk@users.noreply.github.com>
2019-10-01 18:04:11 -0700

    Update ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -12,6 +12,8 @@
 
 #include "libft.h"
 
+/* Function converts ASCII string to integer*/
+
 int	ft_atoi(const char *str)
 {
 	int	i;
@@ -21,12 +23,15 @@ int	ft_atoi(const char *str)
 	i = 0;
 	nbr = 0;
 	sign = 1;
+	/*Skip a char if it is a whitespace */
 	while (str[i] == ' ' || str[i] == '\n' || str[i] == '\r' || str[i] == '\t'
 			|| str[i] == '\v' || str[i] == '\f')
 		i++;
+	/*Skip if "+" sign before number. If "-" sign, store the triger to return negative number*/
 	if (str[i] == '-' || str[i] == '+')
 		if (str[i++] == '-')
 			sign = -1;
+	/*If a char is a digit convert to integer type*/
 	while (str[i] >= '0' && str[i] <= '9')
 		nbr = (nbr * 10) + (str[i++] - '0');
 	return (nbr * sign);
